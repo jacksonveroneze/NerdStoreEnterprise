400a23049b9f14c13c11e38c6acc5b6bcadce15c
Jackson Veroneze <jackso@jacksonveroneze.com>
2020-05-17 19:15:53 -0300

    First commit.

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NSE.Identidade.API.Extensions;
+using NSE.Identidade.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    [Route("[api/identidade]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                      UserManager<IdentityUser> userManager,
+                      IOptions<AppSettings> appSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<IActionResult> Registrar(UsuarioLogin command)
+        {
+            if (ModelState.IsValid is false) return BadRequest();
+
+            IdentityUser identityUser = new IdentityUser
+            {
+                UserName = command.Email,
+                Email = command.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, command.Senha);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<IActionResult> Login(UsuarioLogin command)
+        {
+            if (ModelState.IsValid is false) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(command.Email, command.Senha, false, true);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest();
+        }
+    }
+}
