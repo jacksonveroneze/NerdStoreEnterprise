59cc601e70c07720f754e407c56a2391a4db0e37
Jackson Veroneze <jackson@jacksonveroneze.com>
2020-05-18 18:15:44 -0300

    Ajustes na API de identidade.

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -23,7 +23,6 @@ namespace NSE.Identidade.API
             services.AddSwaggerConfiguration();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwaggerConfiguration();
